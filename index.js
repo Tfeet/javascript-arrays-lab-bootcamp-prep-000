9b3be9f7a9fc56337819a6f98ce5e322632fc857
Rebecca Sorrell <rebeccassorrell@gmail.com>
2021-01-12 22:26:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 var kittens = [] //define your array here
 
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependKitten(element) {
+ kittens.unshift(element);
+ return kittens;
+}
+
+function destructivelyAppendKitten(element) {
+ kittens.push(element);
+ return kittens;
+}
